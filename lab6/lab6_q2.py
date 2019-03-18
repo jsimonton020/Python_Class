3190225f93c42e6d697d6e1caffa605185b21c3a
jsimonton020 <jsimonton020@gmail.com>
2019-03-17 22:05:33 -0700

    more lab6

--- lab6/lab6_q2.py
+++ lab6/lab6_q2.py
@@ -1,23 +1,24 @@
 def print_chars(ch1, ch2, num_per_line):
-    a = ord(ch1) # Get the decimal value of ch1 (1 = 49 in ASCii)
-    b = ord(ch2) # Get the decimal value of ch2 (Z = 90 in ASCii)
-    c = (b - a) + 1 # Find the difference between a and b,
-                    # so we know how many times to run the loop
-                    # 49 - 90 = 41, if we don't add 1 it will stop at 'Y'
-    y = 1 # Counter to keep track
+    d_ch1 = ord(ch1)  # Get the decimal value of ch1
+    d_ch2 = ord(ch2)  # Get the decimal value of ch2
+    # range to know how many times to run the loop
+    # counter to keep track of how many times the loop ran
+    range = (d_ch2 - d_ch1) + 1
+    counter = 1
 
-    while y <= c:
-        if y % num_per_line == 0:
-            # If y divides into num_per_line evenly, print on a new line,
-            # this will happen N(th) time depending on the argument given
-            print(chr(a))
+    while counter <= range:
+        if counter % num_per_line == 0:
+            # If counter divides into num_per_line evenly, print on a new line
+            # this will happen Nth time depending on the argument given
+            print(chr(d_ch1))
         else:
             # Otherwise print on the same line with a space
-            print(chr(a), end = " ")
-        # Add 1 to 'a', this will represent the next value on the ASCii table
-        a += 1
+            print(chr(d_ch1), end=" ")
+        # Add 1, this will represent the next value on the ASCii table
+        d_ch1 += 1
         # Increase counter by 1
-        y += 1
+        counter += 1
+
 
 def main():
     print_chars("1", "Z", 10)
